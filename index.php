88c8d0436dfc79c5f580f01231fe72496f02d71b
TaufiqHA <htaufiq225@gmail.com>
2022-09-24 20:24:07 +0700

    php $_SERVER

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Coba PHP</title>
+	<title>GLOBAL VARIABLE</title>
 </head>
 <body>
-	<h2>Hello World!</h2>
-	<p>ini adalah paragraf</p>
+	<table border="1">
+		<?php foreach ($_SERVER as $key => $value) : ?>
+			<tr>
+				<td><?php echo $key ?></td>
+				<td><?php echo $value ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
 </body>
 </html>
