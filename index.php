b3e4df345fc07cddb92866f8e2ab7eef7b033e6a
TaufiqHA <htaufiq225@gmail.com>
2022-09-24 20:09:37 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Coba PHP</title>
+</head>
+<body>
+	<h2>Hello World!</h2>
+	<p>ini adalah paragraf</p>
+</body>
+</html>
